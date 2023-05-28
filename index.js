8d649009f364721d11dbbef68009b69224baecef
fallmx <31561277+fallmx@users.noreply.github.com>
2023-05-28 19:43:19 +0300

    Teht 13.4

--- index.js
+++ index.js
@@ -1,9 +1,67 @@
+require('dotenv').config()
 const express = require('express')
+const { Sequelize, DataTypes, Model } = require('sequelize')
+
 const app = express()
+app.use(express.json())
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.send('Testing.')
+const sequelize = new Sequelize(process.env.DATABASE_URL)
+
+class Blog extends Model {}
+
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.TEXT
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  }
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
+})
+
+Blog.sync()
+
+app.get('/api/blogs', async (req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs)
+})
+
+app.post('/api/blogs', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body)
+    res.json(blog)
+  } catch (error) {
+    return res.status(400).json({ error })
+  }
+})
+
+app.delete('/api/blogs/:id', async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id)
+  if (blog) {
+    await blog.destroy()
+    res.status(204).end()
+  } else {
+    res.status(404).end()
+  }
 })
 
 app.listen(port, () => {
