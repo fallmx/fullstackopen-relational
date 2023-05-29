ebbdfe9a782298b8d2083d17bc0a62b0f1a8f0a6
fallmx <31561277+fallmx@users.noreply.github.com>
2023-05-30 02:48:32 +0300

    Teht 13.22

--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -2,9 +2,27 @@ const router = require('express').Router()
 
 const { Reading } = require('../models')
 
+const tokenExtractor = require('../util/tokenExtractor')
+
 router.post('/', async (req, res) => {
   const reading = await Reading.create(req.body)
   res.json(reading)
+})
+
+router.post('/:id', tokenExtractor, async (req, res) => {
+  const reading = await Reading.findByPk(req.params.id)
+
+  if (!reading) return res.status(404).end()
+
+  if (reading.userId !== req.decodedToken.id) return res.status(401).json({ error: 'not the owner' })
+
+  const newRead = req.body.read
+
+  if (newRead === undefined) throw Error('missing field read')
+
+  reading.read = newRead
+  await reading.save()
+  res.json({ read: newRead })
 })
 
 module.exports = router
