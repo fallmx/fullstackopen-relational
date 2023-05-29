6e941adfd147f2fc3f2b6090ce0e1e033832b190
fallmx <31561277+fallmx@users.noreply.github.com>
2023-05-30 00:17:15 +0300

    Teht 13.17

--- util/db.js
+++ util/db.js
@@ -1,11 +1,36 @@
 const Sequelize = require('sequelize')
 const { DATABASE_URL } = require('./config')
+const { Umzug, SequelizeStorage } = require('umzug')
 
 const sequelize = new Sequelize(DATABASE_URL)
+
+const migrationConf = {
+  migrations: {
+    glob: 'migrations/*.js',
+  },
+  storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
+  context: sequelize.getQueryInterface(),
+  logger: console,
+}
+
+const runMigrations = async () => {
+  const migrator = new Umzug(migrationConf)
+  const migrations = await migrator.up()
+  console.log('Migrations up to date', {
+    files: migrations.map((mig) => mig.name),
+  })
+}
+
+const rollbackMigration = async () => {
+  await sequelize.authenticate()
+  const migrator = new Umzug(migrationConf)
+  await migrator.down()
+}
 
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate()
+    await runMigrations()
     console.log('Connected to Postgres')
   } catch (error) {
     console.error('Connection to Postgres failed:', error)
@@ -13,4 +38,4 @@ const connectToDatabase = async () => {
   }
 }
 
-module.exports = { connectToDatabase, sequelize }
+module.exports = { connectToDatabase, sequelize, rollbackMigration }
